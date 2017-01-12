453a0d9fae50874f83a38c8cc39ff7ba82634be4
soosmile <1813843870@qq.com>
2017-01-12 11:01:19 +0800

    first commit

---
+++ graphicTutorials/MainGame.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <SDL/SDL.h>
+#include <GL/glew.h>
+enum class GameState {PALY,EXIT};
+
+class MainGame
+{
+public:
+	MainGame();
+	~MainGame();
+
+	void run();
+	
+
+private:
+	void initSystems();
+	void gameLoop();
+	void processInput();
+	void drawGame();
+
+	SDL_Window* _window;
+	int _screenWidth;
+	int _screenHeight;
+	GameState _gameState;
+
+};
+
---
+++ graphicTutorials/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "MainGame.h"
+
+int main(int argc,char** argv) {
+	MainGame mainGame;
+	mainGame.run();
+	return 0;
+}
